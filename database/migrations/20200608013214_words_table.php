f69f7ca315c323c86397137d17703b58fcb9ee92
Axel Espinosa <axeldavid45@gmail.com>
2020-06-07 20:36:22 -0500

    Creamos la migracion de words

---
+++ database/migrations/20200608013214_words_table.php
@@ -0,0 +1,19 @@
+<?php
+
+use Phinx\Migration\AbstractMigration;
+
+class WordsTable extends AbstractMigration
+{
+    public function change()
+    {
+        $table = $this->table('words',
+                              ['collation' => 'utf8mb4_general_ci']);
+        $table->addColumn('word','string')
+            ->addColumn('picture', 'string')
+            ->addColumn('typeof', 'string')
+            ->addColumn('views', 'biginteger')
+            ->addColumn('workspace_id', 'integer')
+            ->addForeignKey('workspace_id', 'workspaces', 'id', ['delete' => 'CASCADE'])
+            ->create();
+    }
+}
